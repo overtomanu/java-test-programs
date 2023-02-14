7c8103469f736eb4fb9abf44a9b067f77a29dbad
Manohar Bhat <overtomanu@outlook.com>
2023-02-15 00:06:36 +0530

    added more printing behaviors for toString of arrays and lists

--- src/ToStringOfDataTypes.java
+++ src/ToStringOfDataTypes.java
@@ -2,6 +2,9 @@
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ToStringOfDataTypes
 {
@@ -16,5 +19,24 @@ public class ToStringOfDataTypes
 		System.out.println("Long:"+long1);
 		System.out.println("Bigdecimal:"+bigDecimal);
 		System.out.println("Timestamp nano second removed-"+timestamp.toString().split("\\.")[0]);
+		System.out.println("int array:"+new int[] {34,44,56});
+		System.out.println("Integer array:"+new Integer[] {34,44,56});
+		System.out.println(Arrays.deepToString(new int[][] {{3,2,1},{99,54,63},{5}}));
+		System.out.println(Arrays.toString(new int[] {8,76,56,44}));
+		
+		// printing lists
+		// arraylist toString prints elements
+		List<Integer> arrlist = new ArrayList<>();
+		arrlist.add(1);
+		arrlist.add(7);
+		arrlist.add(13);
+		System.out.println("arrlist:"+arrlist);
+		
+		// whereas list returned by Arrays.asList doesn't print it
+		// it seems to print object hash for primitive array
+		System.out.println("using Arrays.asList:"+Arrays.asList(new int[] {1,7,14}));
+		// but prints correctly for object array
+		System.out.println("using Arrays.asList:"+Arrays.asList(new Integer[] {1,7,14}));
+		
 	}
 }
