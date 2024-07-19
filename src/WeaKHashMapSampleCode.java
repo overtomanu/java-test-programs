6ef48b6f0849bc83ded8f9aafcfef1cabaa6aa50
Manohar Bhat <mbhatb@smartshift.com>
2024-07-19 12:39:52 +0530

    added WeaKHashMapSampleCode.java

---
+++ src/WeaKHashMapSampleCode.java
@@ -0,0 +1,28 @@
+import java.util.WeakHashMap;
+
+public class WeaKHashMapSampleCode {
+
+    public static void main(String[] args) {
+        // Creating WeakHashMap of numbers
+        WeakHashMap<String, Integer> numbers = new WeakHashMap<>();
+
+        String two = new String("Two");
+        Integer twoValue = 2;
+        String four = new String("Four");
+        Integer fourValue = 4;
+
+        // Inserting elements
+        numbers.put(two, twoValue);
+        numbers.put(four, fourValue);
+        System.out.println("WeakHashMap: " + numbers);
+
+        // Make the reference null
+        two = null;
+
+        // Perform garbage collection
+        System.gc();
+
+        System.out.println("WeakHashMap after garbage collection: " + numbers);
+    }
+
+}
